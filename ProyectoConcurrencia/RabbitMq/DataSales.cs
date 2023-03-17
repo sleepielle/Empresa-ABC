91c555cb3049193d1cf5b472825110703cc1e8fd
ayestas <ayestaskaroll@unitec.edu>
2023-03-16 23:35:25 -0600

    Agregar Recolector

---
+++ ProyectoConcurrencia/RabbitMq/DataSales.cs
@@ -0,0 +1,14 @@
+﻿namespace ProyectoConcurrencia
+{
+    public class DataSales
+    {
+        public string username { get; set; }
+        public string car_id { get; set; }
+        public string price { get; set; }
+        public string vin { get; set; }
+        public string buyerFirstN { get; set; }
+        public string buyerLastN { get; set; }
+        public string buyersId { get; set; }
+        public string divisionId { get; set; }
+    }
+}
